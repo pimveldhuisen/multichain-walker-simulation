1c966123cd61a5db3fc84eb7313956ad8acc0cd6
Pim <pim.veldhuisen@gmail.com>
2016-09-09 16:40:35 +0200

    Implement a simplified version of walking

--- node.py
+++ node.py
@@ -2,18 +2,44 @@ import random
 
 
 class Node:
-    def __init__(self, id, simulation):
-        self.id = id
+    def __init__(self, identity, simulation):
+        self.identity = identity
         self.simulation = simulation
         self.live_edges = []
         self.blocks = []
 
     def send_identity(self, target):
-        self.simulation.add_event(random.randint(0, 500), target.receive_identity, [self.id])
+        self.simulation.add_event(self.simulation.connection_delay(), target.receive_identity, [self])
 
     def receive_identity(self, sender):
         self.live_edges.append(sender)
 
     def set_blocks(self, blocks):
         self.blocks = blocks
-        print "I have " + str(len(blocks)) + " blocks"
+
+    def take_walk_step(self):
+        if self.live_edges:
+            peer = random.choice(self.live_edges)
+            self.send_introduction_request(peer)
+        self.simulation.add_event(5000, self.take_walk_step)
+
+    def send_introduction_request(self, target):
+        self.simulation.add_event(self.simulation.connection_delay(), target.receive_introduction_request, [self])
+
+    def receive_introduction_request(self, sender):
+        self.send_introduction_response(sender)
+
+    def send_introduction_response(self, target):
+        if self.live_edges:
+            peer = random.choice(self.live_edges)
+            #TODO: while peer eligble to walk
+            self.simulation.add_event(self.simulation.connection_delay(), target.receive_introduction_response, [peer])
+        else:
+            print "I have no live edges"
+
+    def receive_introduction_response(self, peer):
+        self.live_edges.append(peer)
+        self.simulation.add_event(57500, self.live_edge_timeout, [peer])
+
+    def live_edge_timeout(self, peer):
+        self.live_edges.remove(peer)
--- simulation.py
+++ simulation.py
@@ -11,22 +11,30 @@ class Simulation:
         self.event_queue = []
         self.time = 0
 
-    def add_event(self, delta_time, function, arguments):
+    def add_event(self, delta_time, function, arguments=[]):
         event = [self.time + delta_time, function] + arguments
         self.event_queue.append(event)
         self.event_queue.sort(key=lambda x: x[0])
 
+    @staticmethod
+    def connection_delay():
+        return random.randint(100, 500)
+
     def start(self):
+        print "Reading multichain database.."
         database = Database("multichain.db")
         ids = database.get_identities()
-
         for i in range(len(ids)):
             node = Node(i+1, self)
             node.set_blocks(database.get_blocks(ids[i]))
+            node.live_edges.append(self.bootstrap)
+            node.send_identity(self.bootstrap)
             self.nodes.append(node)
-            self.add_event(random.randint(0, 500), node.send_identity, [self.bootstrap])
+            self.add_event(Simulation.connection_delay(), node.take_walk_step)
+
+        print "Starting simulation.."
         while self.event_queue:
-            if self.time == self.max_time:
+            if self.time < self.max_time:
                 event = self.event_queue.pop(0)
                 self.time = event[0]
                 print "Time: " + str(self.time) + " | " + str(event[1:])
@@ -37,6 +45,3 @@ class Simulation:
 
         print "No more events"
         return
-
-
-
