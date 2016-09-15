98051cbd9f7dc644225ed196181e1a7635842a09
Pim <pim.veldhuisen@gmail.com>
2016-09-16 01:21:48 +0200

    Add plotting of blocks over time

---
+++ plot/plot_blocks.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+with open("blocks.dat") as f:
+	data = np.loadtxt(f)
+
+timestamps = data[:,0]
+values = data[:,1:]
+averages = np.average(values, axis=1)
+
+plt.clf()
+plt.boxplot(values.transpose(), showmeans=True, showfliers=False)
+plt.show()
