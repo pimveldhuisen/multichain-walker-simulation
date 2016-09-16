3722bc09a51048597772cc288f8c38add739bfb6
Pim <pim.veldhuisen@gmail.com>
2016-09-16 13:29:26 +0200

    Add support for command line arguments

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+#!/usr/bin/python
+
+import argparse
+
 from simulation import Simulation
 
-Simulation(360000, "nodes/results.dat").start()
+parser = argparse.ArgumentParser(description='Simulate a run of the multichain walker.')
+parser.add_argument('-t', '--time', default=60000, help='Time to run the simulation for in miliseconds', type=int)
+parser.add_argument('-f', '--file', default='nodes/results.dat', help='Output file for the simulation')
+args = parser.parse_args()
+
+Simulation(args.time, args.file).start()
