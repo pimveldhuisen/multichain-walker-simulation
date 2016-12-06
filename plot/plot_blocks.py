ea0ecc50b21f8295d3af14c33d6bbfc29d6c4dad
Pim <pim.veldhuisen@gmail.com>
2016-12-06 15:57:58 +0100

    Minor fixes in block plot script

--- plot/plot_blocks.py
+++ plot/plot_blocks.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 with open("blocks.dat") as f:
-	data = np.loadtxt(f)
+    data = np.loadtxt(f)
 
-timestamps = data[:,0]
-values = data[:,1:]
+timestamps = data[:, 0]
+values = data[:, 1:]
 averages = np.average(values, axis=1)
 
 plt.figure(figsize=(20,20))
 plt.boxplot(values.transpose(), showmeans=True, showfliers=False)
-plt.savefig('blocks.png', dpi=200)
+plt.savefig('blocks.png', dpi=50)
